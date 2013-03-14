d6a76e43bc6867879139e635cdfddc8bb0567b37
Salvatore Borgia <salvo2789@gmail.com>
2013-03-14 21:17:36 +0100

    bug fix

--- parseZip.py
+++ parseZip.py
@@ -1,26 +1,27 @@
 import re,struct,sys
 
 def extractCdh(zipFile):
-	indList = [0]
-	while True:
-		try:
-			indList.append(zipFile.index("\x50\x4b\x01\x02", indList[-1]+1, len(zipFile))) 
-		except:
-			break
-	indList = indList[1:]
-	indList.append(len(zipFile))
-	return [zipFile[indList[i]:indList[i+1]] for i in range(len(indList)-1)]
-		
+    indList = [0]
+    while True:
+        try:
+            indList.append(zipFile.rindex(bytes("\x50\x4b\x01\x02","ascii"), indList[-1]+1, len(zipFile)))
+        except:
+            break
+    print(indList)
+    indList = indList[1:]
+    indList.append(len(zipFile))
+    return [zipFile[indList[i]:indList[i+1]] for i in range(len(indList)-1)]
+
 def createCdhEntry(cdh_entry):
 	d = {}
-	d['signature'] = cdh_entry[0:4].encode('hex')
+	d['signature'] = cdh_entry[0:4]#.encode('hex')
 	d['version'] = struct.unpack("<h", cdh_entry[4:6])[0]
 	d['version_min'] = struct.unpack("<h", cdh_entry[6:8])[0]
 	d['flags'] = struct.unpack("<h", cdh_entry[8:10])[0]
 	d['compression'] = struct.unpack("<h", cdh_entry[10:12])[0]
 	d['mod_time'] = struct.unpack("<h", cdh_entry[12:14])[0]
 	d['mod_date'] = struct.unpack("<h", cdh_entry[14:16])[0]
-	d['crc32'] = cdh_entry[16:20].encode('hex')
+	d['crc32'] = cdh_entry[16:20]#.encode('hex')
 	d['compressed_size'] = struct.unpack("<I", cdh_entry[20:24])[0]
 	d['uncompressed_size'] = struct.unpack("<I", cdh_entry[24:28])[0]
 	d['filename_length'] = struct.unpack("<h", cdh_entry[28:30])[0]
@@ -44,9 +45,10 @@ def printCdh(f):
 		print(createCdhEntry(entry))
 
 def getCdhEntry(zipname, filename):
-	cdh = extractCdh(open(zipname).read())
-	return [createCdhEntry(entry) for entry in cdh if createCdhEntry(entry)['filename'] == filename][0]
-	
+    cdh = extractCdh(open(zipname, "rb").read())
+    print(cdh)
+    return [createCdhEntry(entry) for entry in cdh if createCdhEntry(entry)['filename'] == filename][0]
+
 if __name__ == "__main__":
-	printCdh(open(sys.argv[1]).read())
-	print(getCdhEntry('crypted.zip','plain')['crc32'])
+	printCdh("zippo.zip")
+	print(getCdhEntry('zippo.zip','zipcracker_arg.py')['crc32'])
