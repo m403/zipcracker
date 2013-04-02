2511c33fc0fa1d16a7fee69c219fd03c48f63790
root <mauromatteo.cascella@gmail.com>
2013-04-02 16:48:51 +0200

    update essential zipcracker_threads.c

--- zipcracker_threads.c
+++ zipcracker_threads.c
@@ -2,91 +2,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "contrib/minizip/unzip.h"
 
 #define DEBUG 0
-#define NUM_THREADS 10
-#define CHUNK 50000
+#define NUM_THREADS 20
+#define CHUNK 1000
 #define MAX_WORD_LENGTH 50
-#define NOT_FOUND -3
+#define FILLED -3
 #define FOUND -2
 #define WAIT -1
 
-struct data
-{
-    char *zipfilename;
-    char *dictname;
-}; 
-struct data info;
+char *zipfilename;
+char **chunks;
+unsigned long num_pwd = 0;
 
-int signal = WAIT;
-int global_pos = 0;
-int thread_term;
-int extract_pwd;
+/* mutex on num_pwd variable */
+pthread_mutex_t pwd_mutex;
 
 /* mutex and condition variable between main thread and worker threads */
-pthread_mutex_t signal_mutex;
-pthread_cond_t signal_cond;
-/* mutex on extract_pwd variable */
-pthread_mutex_t extract_mutex;
-/* mutex on global_pos variable */
-pthread_mutex_t position_mutex;
+int main_signal = WAIT;
+pthread_mutex_t main_mutex;
+pthread_cond_t main_cond;
+
+/* mutex and condition variable between main thread and worker threads */
+int thread_signal = WAIT;
+pthread_mutex_t thread_mutex;
+pthread_cond_t thread_cond;
 
 /* prototypes */
 void *worker(void *);
 char *readline(char *);
 int extract(unzFile, char *);
-void send_signal(int);
+int fill_chunk(char *, FILE *, int);
+void send_signal_main(int);
+void send_signal_thread(int);
 
 int main (int argc, char *argv[])
 {
-    int i;
-    info.zipfilename = argv[1];
-    info.dictname = argv[2];
+    int i, position = SEEK_SET;
+    FILE *fp_dict = fopen(argv[2], "r");
+    time_t start,end;
+    start = time(NULL);
+    zipfilename = argv[1];
+    chunks = calloc(NUM_THREADS, sizeof(char *));
     pthread_t workers[NUM_THREADS];
     pthread_attr_t attr;
 
+    for (i=0; i<NUM_THREADS; i++)
+    {
+        chunks[i] = calloc(CHUNK, sizeof(char));
+        fill_chunk(chunks[i], fp_dict, position);
+        position += CHUNK;
+    }
+
     /* Initialize mutex and condition variable objects */
-    pthread_mutex_init(&position_mutex, NULL);
-    pthread_mutex_init(&extract_mutex, NULL);
-    pthread_mutex_init(&signal_mutex, NULL);
-    pthread_cond_init(&signal_cond, NULL);
+    pthread_mutex_init(&pwd_mutex, NULL);
+    pthread_mutex_init(&main_mutex, NULL);
+    pthread_cond_init(&main_cond, NULL);
+    pthread_mutex_init(&thread_mutex, NULL);
+    pthread_cond_init(&thread_cond, NULL);
 
     /* For portability, explicitly create threads in a joinable state */
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    
+
     for (i=0; i<NUM_THREADS; i++) 
         pthread_create(&workers[i], &attr, worker, (void *)i);
 
     while (1) 
     {
         /* wait signal from worker threads */
-        pthread_mutex_lock(&signal_mutex);
-        while (signal == WAIT) 
+        pthread_mutex_lock(&main_mutex);
+        while (main_signal == WAIT) 
         {
             if (DEBUG)
                 printf("Main(): Waiting signal\n");
-            pthread_cond_wait(&signal_cond, &signal_mutex);
+            pthread_cond_wait(&main_cond, &main_mutex);
         }
         /* signal received */
-        if (signal == NOT_FOUND) 
-        {
-            printf("[-] PASSWORD NOT FOUND\n");
-            break;
-        }
-        else if (signal == FOUND)
+        if (main_signal == FOUND)
             break;
         /* the signal received is the index of the terminated thread */
         if (DEBUG)
-            printf("Main(): Spawn new thread %d\n", signal);
-        if (pthread_create(&workers[signal], &attr, worker, (void *)signal))
-        {
-            printf("Max thread limit!\n");
-            return -1;
-        }
-        signal = WAIT;
-        pthread_mutex_unlock(&signal_mutex);
+            printf("Main(): Worker-thread %d finished, re-fill buffer\n", main_signal);
+        chunks[main_signal] = realloc(chunks[main_signal], CHUNK);
+        fill_chunk(chunks[main_signal], fp_dict, position);
+        position += CHUNK;        
+        if (DEBUG)
+            printf("Main(): Send FILLED signal to Worker-thread %d\n", main_signal);
+        send_signal_thread(FILLED);
+        main_signal = WAIT;
+        pthread_mutex_unlock(&main_mutex);
     }
 
     if (DEBUG)
@@ -94,82 +101,93 @@ int main (int argc, char *argv[])
 
     /* Clean up and exit */
     pthread_attr_destroy(&attr);
-    pthread_mutex_destroy(&signal_mutex);
-    pthread_mutex_destroy(&extract_mutex);
-    pthread_mutex_destroy(&position_mutex);
-    pthread_cond_destroy(&signal_cond);
+    pthread_mutex_destroy(&pwd_mutex);
+    pthread_mutex_destroy(&main_mutex);
+    pthread_cond_destroy(&main_cond);
+    pthread_mutex_destroy(&thread_mutex);
+    pthread_cond_destroy(&thread_cond);
+    fclose(fp_dict);
+    end = time(NULL);
+    printf("Tempo tot. %lu\n", end-start);
+    printf("Password per sec. %lu\n", num_pwd/(end-start));
     return 1;    
 }
 
-void send_signal(int value)
+int fill_chunk(char *buf, FILE *desc, int pos) 
 {
-    pthread_mutex_lock(&signal_mutex);
-    signal = value;
-    pthread_cond_signal(&signal_cond);
-    pthread_mutex_unlock(&signal_mutex);
+    fseek(desc, 0, pos);
+    return fread(buf, 1, CHUNK, desc);
+}
+
+void send_signal_main(int value)
+{
+    pthread_mutex_lock(&main_mutex);
+    main_signal = value;
+    pthread_cond_signal(&main_cond);
+    pthread_mutex_unlock(&main_mutex);
+}
+
+void send_signal_thread(int value)
+{
+    pthread_mutex_lock(&thread_mutex);
+    thread_signal = value;
+    pthread_cond_signal(&thread_cond);
+    pthread_mutex_unlock(&thread_mutex);
 }
 
 void *worker(void *index)
 {
+    //printf("%s\n---------\n", words);
     if (DEBUG)
         printf("Worker-thread() %d: Started\n", (int)index);
 
-    FILE *fp_dict;
-    char *zname = info.zipfilename;
-    char *dict = info.dictname;
     char *password = (char*)calloc(MAX_WORD_LENGTH, sizeof(password));
-    char *buf = (char *)calloc(CHUNK, sizeof(char)), *tmp = buf;
-    int pos;
-    unzFile uf = unzOpen64(zname);
+    char *words, *tmp; 
+    unzFile uf = unzOpen64(zipfilename);
     if(uf == NULL)
     {
-        printf("[-] ERROR: Failed to open %s\n", zname);
+        printf("[-] ERROR: Failed to open %s\n", zipfilename);
         pthread_exit(NULL);
     }
-    /* access global_pos variable */
-    pthread_mutex_lock(&position_mutex);
-    pos = global_pos;
-    global_pos += CHUNK;
-    pthread_mutex_unlock(&position_mutex);
-
-    fp_dict = fopen(dict, "r");
-    fseek(fp_dict, pos, 0);
-    if(!fread(buf, 1, CHUNK, fp_dict))
-    {
-        printf("[-] ERROR: Failed to read dictionary file\n");
-        pthread_exit(NULL);
-    }
-    /* read passwords until end of CHUNK */
-    while((password = readline(tmp)) && (int)tmp < (int)buf+CHUNK)
-    {
-        /* printf("%s\n", password); */
-        pthread_mutex_lock(&extract_mutex);
-        if(extract(uf, password) == UNZ_OK)
+    while (1) {
+        words = chunks[(int)index];
+        tmp = words;
+        //read passwords until end of CHUNK
+        while((password = readline(tmp)) && (int)tmp < (int)words+CHUNK)
         {
-            printf("[+] PASSWORD FOUND: %s\n", password);
-            fclose(fp_dict);
+            pthread_mutex_lock(&pwd_mutex);
+            num_pwd += 1;
+            pthread_mutex_unlock(&pwd_mutex);
+            if(extract(uf, password) == UNZ_OK)
+            {
+                printf("[+] PASSWORD FOUND: %s\n", password);
+                free(password);
+                unzCloseCurrentFile(uf);
+                if (DEBUG)
+                    printf("Worker-thread() %d: Send signal FOUND to Main thread\n", (int)index);
+                send_signal_main(FOUND);
+                pthread_exit(NULL);
+            }
+            tmp += strlen(password)+1;
             free(password);
-            free(buf);
-            unzCloseCurrentFile(uf);
-            pthread_mutex_unlock(&extract_mutex);
-            if (DEBUG)
-                printf("Worker-thread() %d: Send signal FOUND to Main thread\n", (int)index);
-            send_signal(FOUND);
-            pthread_exit(NULL);
         }
-        tmp += strlen(password)+1;
+        //current thread has not found the password
         free(password);
-        pthread_mutex_unlock(&extract_mutex);
+        unzCloseCurrentFile(uf);
+        if (DEBUG)
+            printf("Worker-thread() %d: Send signal to Main thread\n", (int)index);
+        send_signal_main((int)index);
+
+        pthread_mutex_lock(&thread_mutex);
+            while (thread_signal == WAIT) 
+            {
+                if (DEBUG)
+                    printf("Main(): Waiting signal\n");
+                pthread_cond_wait(&thread_cond, &thread_mutex);
+            }
+        thread_signal = WAIT;
+        pthread_mutex_unlock(&thread_mutex);
     }
-    /* current thread has not found the password */
-    fclose(fp_dict);
-    free(password);
-    free(buf);
-    unzCloseCurrentFile(uf);
-    if (DEBUG)
-        printf("Worker-thread() %d: Send signal to Main thread\n", (int)index);
-    send_signal((int)index);
-    pthread_exit(NULL);
 }
 
 char *readline(char *string) 
